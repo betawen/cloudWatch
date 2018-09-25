e8b198ac94ec5ab88db10967627fefcdba709c96
jing <jing_find@163.com>
2018-09-25 20:29:31 +0800

    new watch

---
+++ cloudWatchSample3.js
@@ -0,0 +1,42 @@
+let AWS=require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
+
+let cloudWatch;
+
+function putMetricData(name,value){
+    if(typeof value==='undefined' || typeof name === 'undefined' || !Number.isInteger(value) || value<0){
+        console.log("Invailed params.");
+    }
+    if(!cloudWatch){
+        cloudWatch=new AWS.CloudWatch();
+    }
+    console.log("Params vailed");
+
+    let params={
+        MetricData:[{
+            MetricName:name,
+            Timestamp:new Date,
+            Value:value
+        }],
+        NameSpace:'user-count'
+    };
+
+    return new Promise((resolve,reject)=>{
+        params.MetricData[0].Value=value;
+        params.MetricData[0].Timestamp=new Date();
+
+        cloudWatch.putMetricData(params,(err,data)=>{
+            if(err){
+                console.log("Error: sending metrics to cloudWatch");
+                console.log(err,err.stack);
+                console.log(err.stack);
+            }else{
+                console.log("Sending metrics to cloudWatch successfully! ");
+                console.log(data);
+                resolve(data);
+            }
+        });
+    });
+}
+
+exports.putMetricData=putMetricData;
