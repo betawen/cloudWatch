fa4550abbcf6b264f8d2122a47fa04da92fe4865
jing <jing_find@163.com>
2018-09-25 18:15:04 +0800

    change test1

--- test1.js
+++ test1.js
@@ -1,3 +1,14 @@
+
+let express=require('express');
+let app=express();
+let cookieParser=require('cookie-parser')
+let bodyParser=require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cookieParser());
+
+
 let cloud=require('./cloudWatchSample2')
 
 let url;
@@ -8,3 +19,7 @@ app.get('/hello',(req,res)=>{
     count++;
     res.send("helloworld");
 })
+
+app.listen(8009,()=>{console.log("running on http://localhost:8009")});
+
+module.exports=app;
